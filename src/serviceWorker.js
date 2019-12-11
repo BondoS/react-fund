8dffa2eebc87440f7cd953c75de3e8c888b37edd
BondoS <eng.serag00@gmail.com>
2019-12-11 11:32:38 +0100

    Install jest and enzyme, edit App.test

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -9,7 +9,8 @@
 
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
-
+/* eslint-disable */
+// prettier-ignore
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
