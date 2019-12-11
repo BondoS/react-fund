69b371d3d895312c3df412b781b984fb3b0e7312
BondoS <eng.serag00@gmail.com>
2019-12-12 00:44:07 +0100

    Add loading effect

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
+import LoadingBar from 'react-redux-loading-bar';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
+    <LoadingBar />
     <App />
   </Provider>,
   document.getElementById('root')
--- src/store/sagas/worker.js
+++ src/store/sagas/worker.js
@@ -1,4 +1,5 @@
 import { put } from 'redux-saga/effects';
+import { showLoading, hideLoading } from 'react-redux-loading-bar';
 import getConsumers from '../../services/consumers';
 import {
   consumerImportFail,
@@ -9,11 +10,14 @@ import {
 /* eslint-disable */
 export function* consumersImportSage() {
   try {
+    yield put(showLoading());
     yield put(consumerImportStart());
     const res = yield getConsumers();
     yield put(consumerImportSuccess(res));
+    yield put(hideLoading());
   } catch (error) {
     console.log(error);
     yield put(consumerImportFail(error));
+    yield put(hideLoading());
   }
 }
